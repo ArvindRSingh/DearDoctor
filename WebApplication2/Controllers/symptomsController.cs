b6ab2dd1ad8c6754d93a1eab55f94a76809b0040
Arvind Singh <Arvind@thinkbridge.in>
2018-03-19 00:09:57 +0530

    Initial commit

---
+++ WebApplication2/Controllers/symptomsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Azure;
+using Microsoft.Framework.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Threading.Tasks;
+using LogLevel = Microsoft.Framework.Logging.LogLevel;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class symptomsController : Controller
+    {
+        readonly CloudStorageAccount storageAccount;
+        readonly CloudTableClient tableClient;
+        CloudTable table;
+
+        public symptomsController()
+        {
+            storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            // Create the table client.
+            tableClient = storageAccount.CreateCloudTableClient();
+
+            // Retrieve a reference to the table.
+            table = tableClient.GetTableReference("symptoms");
+
+            // Create the table if it doesn't exist.
+            table.CreateIfNotExists();
+        }
+
+        // GET: symptoms
+        public ActionResult Index(string[] symptoms, AgeGroup? ageGroup, Gender? gender, Region? region)
+        {
+            var filter = TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, symptoms[0]);
+
+            int symptomsIndex = 1;
+            while (symptomsIndex != symptoms.Length)
+            {
+                filter = TableQuery.CombineFilters(
+                    filter,
+                    TableOperators.Or,
+                    TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, symptoms[1])
+                    );
+                symptomsIndex++;
+            }
+
+            filter = TableQuery.CombineFilters(
+                    filter,
+                    TableOperators.And,
+                    TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, SymptomEntity.GetRowKey(ageGroup, gender, region))
+                    );
+
+            // Create the table query.
+            TableQuery <SymptomEntity> rangeQuery = new TableQuery<SymptomEntity>()
+                .Where(filter);
+
+            // Loop through the results, displaying information about the entity.
+
+            var entities = table.ExecuteQuery(rangeQuery).ToList();
+
+            return View(entities);
+        }
+
+        // GET: symptoms/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: symptoms/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: symptoms/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: symptoms/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: symptoms/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: symptoms/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: symptoms/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication2/Models/Symptom.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public enum AgeGroup
+    {
+        NewBorn,
+        YoungChild,
+        OldChild,
+        Adolesent,
+        YoungAdult,
+        AdultIn30s,
+        AdultIn40s,
+        AdultIn50s,
+        AdultIn60s,
+        Senior
+
+    }
+    public enum Region
+    {
+        NorthAmerica,
+        WesternEurope,
+        EasternEurope,
+        NorthAfrica,
+        EastAfrica,
+        WestAfrica,
+        SouthAfrica,
+        CentralAfrica,
+        EastAsia,
+        SouthAsia,
+        SouthEastAsia,
+        Australia,
+        MiddleEast,
+        Caribbean
+
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+
+    }
+    public class SymptomEntity : TableEntity
+    {
+        public static string GetRowKey(AgeGroup? ageGroup, Gender? gender, Region? region)
+        {
+            return string.Concat(
+                ageGroup.HasValue ? Enum.GetName(typeof(AgeGroup), ageGroup.Value) : "",
+                gender.HasValue ? Enum.GetName(typeof(Gender), gender.Value) : "",
+                region.HasValue ? Enum.GetName(typeof(Region), region.Value) : ""
+                );
+        }
+
+        public SymptomEntity(string symptom, AgeGroup ageGroup, Gender gender, Region region)
+        {
+            this.PartitionKey = symptom;
+            this.RowKey = SymptomEntity.GetRowKey(ageGroup, gender, region);
+        }
+        public SymptomEntity() { }
+        public Gender Gender { get; set; }
+        public Region Region { get; set; }
+        public string LikelyDiagnosis { get; set; }
+
+    }
+}
